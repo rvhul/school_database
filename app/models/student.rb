8c1aab3d4daec2ec38459896f9dc1db4c8d8004c
Rahul Malhotra <rvhul.malhotra@gmail.com>
2016-03-04 03:02:34 +0530

    Add student validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,15 @@
 class Student < ActiveRecord::Base
   belongs_to :section
   belongs_to :house
+
+  validates(:name, presence: true)
+  validates(:address, presence: true)
+  validates(:house_id, presence: true)
+  validates(:section_id, presence: true)
+  validates(:gender, presence: true)
+  validates(:dob, presence: true)
+  validates(:fathers_name, presence: true)
+  validates(:emergency_contact, presence: true)
+  validates(:blood_type, presence: true)
+
 end
