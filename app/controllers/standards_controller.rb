014a6629cb32b0057a498f74c59e850f1078cc10
Rahul Malhotra <rvhul.malhotra@gmail.com>
2016-03-03 19:33:02 +0530

    Add CRUD operations on House. Add Standard model

--- app/controllers/standards_controller.rb
+++ app/controllers/standards_controller.rb
@@ -1,5 +1,36 @@
 class StandardsController < ApplicationController
   def index
+    @standard = Standard.all
+  end
+
+  def new
+    @standard = Standard.new
+  end
+
+  def create
+    @standard = Standard.new(standard_params)
+    if @standard.save
+      redirect_to standards_path
+    else
+      render new
+    end
+  end
+
+  def edit
+    @standard = Standard.find(params[:id])
+  end
+
+  def update
+    @standard = Standard.find(params[:id])
 
   end
+
+  def destroy
+
+  end
+
+private
+def standard_params
+  params.require(:standard).permit(:name)
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303122408) do
+ActiveRecord::Schema.define(version: 20160303140125) do
 
   create_table "houses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "standards", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
