9796b8aa99a2748487435d7377324722b2885f17
Rahul Malhotra <rvhul.malhotra@gmail.com>
2016-03-03 18:32:07 +0530

    Add House delete functionality

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,5 +1,34 @@
 class HousesController < ApplicationController
   def index
+    @house = House.all
+  end
 
+  def new
+    @house = House.new
+  end
+
+  def update
+    @house = House.find(params[:id])
+    @house.update_attribute(:name, params[:name])
+  end
+
+  def create
+    @house = House.new(house_params)
+    if @house.save
+      redirect_to houses_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @house = House.find(params[:id])
+    @house.destroy!
+    redirect_to houses_path
+  end
+
+private
+  def house_params
+    params.require(:house).permit(:name)
   end
 end
