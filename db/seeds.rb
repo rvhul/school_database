7a5a3d61be72eface959a3b9f5bb3fc68399d205
Rahul Malhotra <rvhul.malhotra@gmail.com>
2016-03-04 17:29:02 +0530

    Navbar style revised

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+def seed_standards(name)
+  Standard.find_or_create_by(name: name)
+  print "."
+end
+
+puts "#Seeding Standards"
+["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th"].each do |standard|
+  seed_standards(standard)
+end
+puts "Done."
+
+
+def seed_sections(name, standard_id)
+  @standard = Standard.find(standard_id)
+  Standard.Section.find_or_create_by(name: name, standard_id: standard_id)
+  print "."
+end
+
+puts "#Seeding Sections"
+Standard.all.each do |id|
+  ["A", "B", "C", "D"].each do |section|
+    seed_sections(section,id)
+  end
+end
+puts "Done."
