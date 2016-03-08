1974a6bdd2fa592dfdfa19a921be9ec5742a4536
Dhanunjaya Lakshmi <dhanunjaya.lakshmi60@gmail.com>
2016-03-08 16:36:55 +0530

    Seed file added

--- app/models/standard.rb
+++ app/models/standard.rb
@@ -1,5 +1,6 @@
 class Standard < ActiveRecord::Base
   has_many :sections, dependent: :destroy
+  has_many :students, through: :sections
 
   validates(:name, presence: true)
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
+require 'rubygems'
+require 'faker'
+require 'byebug'
 
 def seed_standards(name)
   Standard.find_or_create_by(name: name)
@@ -13,22 +15,60 @@ def seed_standards(name)
 end
 
 puts "#Seeding Standards"
-["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th"].each do |standard|
+["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"].each do |standard|
   seed_standards(standard)
 end
 puts "Done."
 
-
-def seed_sections(name, standard_id)
-  @standard = Standard.find(standard_id)
-  Standard.Section.find_or_create_by(name: name, standard_id: standard_id)
+def seed_sections(standard, name)
+  # puts "#{standard}"
+  standard.sections.find_or_create_by(name: name)
   print "."
 end
 
 puts "#Seeding Sections"
-Standard.all.each do |id|
-  ["A", "B", "C", "D"].each do |section|
-    seed_sections(section,id)
+Standard.all.each do |standard|
+  sections = %w[A B C]
+  (0..(1+rand(2))).each do |section|
+    name = sections[section]
+    seed_sections(standard, name)
   end
 end
+puts "Done."
+
+def seed_houses(name)
+  House.find_or_create_by(name: name)
+  print "."
+end
+
+puts "#Seeding Houses"
+%w[Yellow Green Red Blue].each do |house|
+  seed_houses(house)
+end
+puts "Done."
+
+def seed_students(section, house, roll_no)
+  last_name = Faker::Name.last_name
+  age = section.standard.name.to_i + 5
+  section.students.find_or_create_by(roll_no: roll_no) do |student|
+    student.name = "#{Faker::Name.first_name} #{last_name}"
+    student.fathers_name = "#{Faker::Name.first_name} #{last_name}"
+    student.gender = Student::Gender.all.sample.code
+    student.dob = Faker::Date.between((age + 1).years.ago, (age - 1).years.ago)
+    student.emergency_contact = Faker::PhoneNumber.cell_phone
+    student.blood_type = Student::BloodType.all.sample.code
+    student.address = "#{Faker::Address.street_address}, #{Faker::Address.city}-#{Faker::Address.zip}"
+    student.house = house
+  end
+  print "."
+end
+
+puts "#Seeding Students"
+  Section.all.each do |section|
+    all_houses = House.all
+    (1..(1+rand(6))).each do |num|
+      house = all_houses.sample
+      seed_students(section, house, num)
+    end
+  end
 puts "Done."
